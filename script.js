b5767767c3ebf6883a827657020381c73881ff8b
lzambrano-ec <55524978+lzambrano-ec@users.noreply.github.com>
2021-07-10 16:32:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+    $(function(){
+  function cargaDeVideos(){
+    $('#video1').attr('src', 'https://www.youtube.com/embed/UBCk4mp1Z6Q?rel=0');
+    $('#map1').attr('src', 'https://www.google.com/maps/embed/v1/place?q=-2.144982%2C%20-79.918700&key=AIzaSyCWQeV1gMbHrr_LXbz1izUmoiL9C1RzsPg');
+  }
 
-    function init() {
-           var vidDefer = document.getElementsByTagName('iframe');
-           for (var i=0; i<vidDefer.length; i++) {
-           if(vidDefer[i].getAttribute('data-src')) {
-           vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
-           } } }
-           window.onload = init;
+  setTimeout(cargaDeVideos);
+
+});
  
